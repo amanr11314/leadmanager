a96f8156092710dbc8232e77eaff8c0a0a8750bb
amanr11314 <amanr11314@gmail.com>
2021-09-27 01:55:07 +0530

    context + reducer Devtools testing

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,9 +3,10 @@ import { render } from "react-dom";
 import Dashboard from "./leads/Dashboard";
 import Header from "./layouts/Header";
 import GlobalState from "../context/GlobalState";
-import { transitions, positions, Provider as AlertProvider } from "react-alert";
+import { Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
 import Alert from "./layouts/Alert";
+import { debugContextDevtool } from "react-context-devtool";
 export default function App() {
   return (
     <GlobalState>
@@ -21,3 +22,6 @@ export default function App() {
 }
 const appDiv = document.getElementById("app");
 render(<App />, appDiv);
+
+//Attach contextDevtool
+debugContextDevtool(appDiv);
