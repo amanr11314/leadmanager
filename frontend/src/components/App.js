da444bc1ecff6aa265eddc92fa11f1a57364b101
amanr11314 <amanr11314@gmail.com>
2021-09-30 21:05:55 +0530

    fixed access to loadUser: component nesting down one level

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,45 +1,17 @@
-import React, { Fragment, useContext } from "react";
+import React from "react";
 import { render } from "react-dom";
 
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
-
-import Dashboard from "./leads/Dashboard";
-import Header from "./layouts/Header";
 import GlobalState from "../context/GlobalState";
 import { Provider as AlertProvider } from "react-alert";
-import PrivateRoute from "./common/PrivateRoute";
-import Login from "./accounts/Login";
-import Register from "./accounts/register";
 import AlertTemplate from "react-alert-template-basic";
-import Alert from "./layouts/Alert";
 import { debugContextDevtool } from "react-context-devtool";
-// import GlobalContext from "../context/lead-context";
+
+import Medium from "./Medium";
 export default function App() {
-  // const context = useContext(GlobalContext);
-
-  // useEffect(() => {
-  //   context
-  //   return () => {
-  //     cleanup
-  //   }
-  // }, [input])
-
   return (
     <GlobalState>
       <AlertProvider template={AlertTemplate}>
-        <Router>
-          <Fragment>
-            <Header />
-            <Alert />
-            <div className="container">
-              <Switch>
-                <PrivateRoute exact path="/" component={Dashboard} />
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/login" component={Login} />
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
+        <Medium />
       </AlertProvider>
     </GlobalState>
   );
---
+++ frontend/src/components/Medium.js
@@ -0,0 +1,33 @@
+import React, { Fragment, useContext, useEffect } from "react";
+
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+
+import Dashboard from "./leads/Dashboard";
+import Header from "./layouts/Header";
+import PrivateRoute from "./common/PrivateRoute";
+import Login from "./accounts/Login";
+import Register from "./accounts/register";
+import Alert from "./layouts/Alert";
+import GlobalContext from "../context/lead-context";
+
+export default function Medium() {
+  const context = useContext(GlobalContext);
+  useEffect(() => {
+    context.loadUser();
+  }, []);
+  return (
+    <Router>
+      <Fragment>
+        <Header />
+        <Alert />
+        <div className="container">
+          <Switch>
+            <PrivateRoute exact path="/" component={Dashboard} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
+  );
+}
